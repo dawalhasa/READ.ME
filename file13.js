8d999b2a58edbe7d5e1967a73853ec5da7e168f9
Dawa Lhasa <dawalhasa@gmail.com>
2022-03-17 02:19:27 +0530

    Enter

--- file13.js
+++ file13.js
@@ -1,5 +1,7 @@
 let myDictionaryTextOne = "word";
 let myDictionarytextTwo = "mening";
-if (myDictionaryTextOne = myDictionarytextTwo){
+if (myDictionaryTextOne == myDictionarytextTwo){
     console.log("meaning");
 }else{
+    console.log("Sorry There is no such word and meaning");
+}
