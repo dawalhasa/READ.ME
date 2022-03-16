72f818ca8ab26fe64c3d3aa683d957443aa5a9bd
Dawa Lhasa <dawalhasa@gmail.com>
2022-03-17 01:34:01 +0530

    Enter

--- file12.js
+++ file12.js
@@ -1,1 +1,35 @@
-console.log("Githubt")
+
+console.log("འདི་རིང་འདི་གར་ང་ཚོ་ Ternary Oparator སྐོར་ལ་སྦྱང་གི་ཡིན་ད།");
+
+// First condition 1st result.
+/* let age = 15;
+let drink ;
+if (age>=5){
+    drink = "coffee";
+}else{
+    drink = "milk";
+}
+console.log(drink); */
+
+// Second condition 2ed result.
+/* let age = 4;
+let drink ;
+if (age>=5){
+    drink = "coffee";
+}else{
+    drink = "milk";
+}
+console.log(drink); */
+
+// So for this long coding U can use Ternary Oparator and we call it conditional oparator too.
+
+// 1st example:
+/* let age = 8;
+let drink = age >=5 ? "Coffee" : "Milk";
+console.log(drink); */
+
+
+// 2ed example:
+let age = 3;
+let drink = age >=5 ? "Coffee" : "Milk";
+console.log(drink);
---
+++ file13.js
@@ -0,0 +1,7 @@
+let myDictionaryTextOne = "word";
+let myDictionarytextTwo = "mening";
+if (myDictionaryTextOne = myDictionarytextTwo){
+    console.log("meaning");
+}else{
+    console.log("Sorry there is no such word");
+}
