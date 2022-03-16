4c090370d3adf58954c843ec055ef040c6427acf
Dawa Lhasa <dawalhasa@gmail.com>
2022-03-17 03:08:45 +0530

    Enter

--- file13.js
+++ file13.js
@@ -1,7 +1,62 @@
-let myDictionaryTextOne = "word";
+/* let myDictionaryTextOne = "word";
 let myDictionarytextTwo = "mening";
 if (myDictionaryTextOne == myDictionarytextTwo){
     console.log("meaning");
 }else{
     console.log("Sorry There is no such word and meaning");
+} */
+
+// Now we are going to discuss about And and Or operator.
+
+// First let us make a variable
+
+let myName = "Dawa";
+let myAge = 43;
+
+/* if (myName[0]==="D"){
+    console.log("Your Name Truely Starts with D ");
 }
+if (myAge>40){
+    console.log("Your Are Not A Gentle-man Now");
+} */
+
+// So here we will use And and Or operator to creat this method in a better and more easy and proper-way.
+
+/* /* if (myName[0] === "D" && myAge > 40){
+    console.log("Your Name Start With D And Your Are No more Gentle-man Now");
+}else{
+    console.log("You Are Smart"); 
+} */
+
+/* if (myName[0] === "d" && myAge > 40){
+    console.log("Your Name Start With D And Your Are No more Gentle-man Now");
+}else{
+    console.log("You Are Smart");
+} */
+
+/* if (myName[0] === "D" && myAge > 50){
+    console.log("Your Name Start With D And Your Are No more Gentle-man Now");
+}else{
+    console.log("You Are Smart");
+} */
+
+/* if (myName[0] === "d" || myAge > 40){
+    console.log("Your Name Start With D And Your Are No more Gentle-man Now");
+}else{
+    console.log("You Are Smart");
+} */
+
+/* if (myName[0] === "D" || myAge > 50){
+    console.log("Your Name Start With D And Your Are No more Gentle-man Now");
+}else{
+    console.log("You Are Smart");
+} */
+
+// if both condition is false then it will give you the falsy result.
+
+/* if (myName[0] === "d" && myAge > 50){
+    console.log("Your Name Start With D And Your Are No more Gentle-man Now");
+}else{
+    console.log("You Are Smart");
+}
+ */
---
+++ file14.js
@@ -0,0 +1,19 @@
+// console.log("Course Title; [Nested if else]");
+
+let winner_Num = 19;
+let User_Guess = prompt("Guess A Number");
+console.log(typeof User_Guess.User_Guess);
+
+
+/* if ("you guess 19"){
+    console.log("You Win");
+}else{
+    if("You Guess Above 19 is too High{
+        else{
+            console.log("You Guess Lower 19 is too Low");
+        }
+    }
+}
+ */
+
+
