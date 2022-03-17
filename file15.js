bc2ae3921ba76cca79534230c190b216ec5aa0c6
Dawa Lhasa <dawalhasa@gmail.com>
2022-03-18 00:13:46 +0530

    Enter

--- file15.js
+++ file15.js
@@ -5,18 +5,18 @@ else if
 else
  */
 
-let temInDegree = 50;
+/* let temInDegree = 50;
 
 if (temInDegree < 0) {
     console.log("Extramely Cold Outside");
 } else if (temInDegree < 16) {
     console.log("It's cold Outside");
 }else if (temInDegree < 25) {
-    console.log("The Wheater is good")
+    console.log("The Wheater is good");
 }else if (temInDegree < 35) {
-    console.log("Let's go for Swim")
+    console.log("Let's go for Swim");
 }else if (temInDegree < 45) {
-    console.log("Put on Ur AC")
+    console.log("Put on Ur AC");
 } else {
-    console.log("It's Extremely Hot Outside")
-}
+    console.log("It's Extremely Hot Outside");
+} */
---
+++ file17.js
@@ -0,0 +1,48 @@
+console.log("Chapter 17 we are going to study about `While Loop`")
+
+// If you need to  print bunch of Number start from 0 to 9.
+
+/* console.log(0)
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log(6)
+console.log(7)
+console.log(8)
+console.log(9)
+ */
+
+// but this same method you can assign very elegently with loop
+
+// In coding language there is a word called dry that means don't repeat.
+
+
+/* let i = 0;
+console.log(i)
+i++;
+console.log(i)
+i++;
+console.log(i)
+i++;
+console.log(i)
+i++;
+console.log(i)
+i++;
+console.log(i)
+i++;
+console.log(i)
+i++;
+console.log(i)
+i++;
+console.log(i)
+i++;
+console.log(i) */
+
+// Now come to the while loop Style
+
+/* let i = 0;
+while (0 <= 3) { */
+    
+
