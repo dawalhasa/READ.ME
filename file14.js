ed1a868263b1122cd72c9c016297a007f3ba0f3d
Dawa Lhasa <dawalhasa@gmail.com>
2022-03-17 16:05:09 +0530

    Enter

--- file14.js
+++ file14.js
@@ -11,11 +11,11 @@ console.log(typeof User_Guess, User_Guess); */
 let winner_Num = 19;
 let User_Guess = +prompt("Guess A Number");
 
- if (User_Guess === winner_Num){
+if (User_Guess === winner_Num) {
     console.log("You Win");
-}else{
-         console.log("You are wrong");
-        }
-   
+} else {
+    console.log("You are wrong");
+}
+
 
 
