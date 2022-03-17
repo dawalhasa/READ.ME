b32357ef74c31e6995b5fe49b449f9f2d63a8756
Dawa Lhasa <dawalhasa@gmail.com>
2022-03-17 16:40:18 +0530

    Enter

--- file15.js
+++ file15.js
@@ -4,3 +4,19 @@ else if
 else if
 else
  */
+
+let temInDegree = 30;
+
+if (temInDegree < 0) {
+    console.log("Extramely Cold Outside");
+} else if (temInDegree < 16) {
+    console.log("It's cold Outside");
+}else if (temInDegree < 25) {
+    console.log("The Wheater is good")
+}else if (temInDegree < 35) {
+    console.log("Let's go for Swim")
+}else if (temInDegree < 45) {
+    console.log("Put on Ur AC")
+} else {
+    console.log("It's Extremely Hot Outside")
+}
