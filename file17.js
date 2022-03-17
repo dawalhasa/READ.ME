33856d4bbfa1040cdba448fe47d55c52d9cc43f2
Dawa Lhasa <dawalhasa@gmail.com>
2022-03-18 00:46:55 +0530

    Enter

--- file17.js
+++ file17.js
@@ -43,6 +43,63 @@ console.log(i) */
 // Now come to the while loop Style
 
 /* let i = 0;
-while (0 <= 3) { */
-    
+while (i <= 9) {
+    console.log(i);
+    i++
+} */
+/* let i = 0;  //1,2,3,4.
+while (i <= 9) {
+    console.log(i);
+    i++
+}
+console.log("Hello!!!"); */
+
+/* let i = 0;  //1,2,3,4.
+while (i<= 9) {
+    console.log(i);
+    i ++ ;
+}
+console.log(`Current Value i is ${i}`);
+console.log("Hello!!!"); */
+
+// Now one another While Loop example:
+
+/* let Num = 10;
+let total = 0;
+total = total + 1;
+total = total + 2;
+total = total + 3;
+total = total + 4;
+total = total + 5;
+total = total + 6;
+total = total + 7;
+total = total + 8;
+total = total + 9;
+total = total + 10;
+console.log(total); */
+
+/* let Num = 10;
+let total = 0;
+total = 0 + 1;
+total = 1 + 2;
+total = 3 + 3;
+total = 6 + 4;
+total = 10 + 5;
+total = 15 + 6;
+total = 21 + 7;
+total = 28 + 8;
+total = 36 + 9;
+total = 45 + 10;
+console.log(total); */
+
+let Num = 10;
+let total = 0;
+let i = 0;
+while (i <= 10) {
+    total = total + i;
+    i++;
+}
+console.log(total);
+
+
 
