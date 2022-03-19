63f01d81745be0844eba3ced718900213e88926b
Dawa Lhasa <dawalhasa@gmail.com>
2022-03-20 03:05:47 +0530

    Enter

--- file18.js
+++ file18.js
@@ -1,1 +1,13 @@
 // console.log("Now We are going to study `For Loop`")
+// console.log(`Tashi Delek Phulsom Tsog Ema Bagdoe KunKham Zang`);
+// Whenever when we need to excute a code in javascript we have to have creat excution context.  
+// that excution context have Global Excution Context.
+// They are two phases in Global Excution Context 
+// 1 Creat Phase 
+// 2 Code Excution Phase 
+// When we created Global Excution for example Var firstName we created variasion and that is Undefined.
+
+console.log(this);
+console.log(window);
+console.log(firstName);
+var firstName = "harshit";
